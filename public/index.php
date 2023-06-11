e67e337f146576a622593468ddfe18bfe302c756
atheo810 <atheos810@gmail.com>
2023-06-11 10:49:49 +0800

    feat: public/index

--- public/index.php
+++ public/index.php
@@ -34,16 +34,51 @@ $paths = new Indoframe\Config\Paths();
 // Location of the Framework bootstrap file.
 require rtrim($paths->appDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-
-$router = require_once APPPATH . '/Routes/routes.php';
-
+// Get Current URI
 $requestUri = $_SERVER['REQUEST_URI'];
+
+// Get path from URI
 $parsedUrl = parse_url($requestUri);
 $path = $parsedUrl['path'];
-$path = $path === '' ? '/' : $path;
-if (strpos($path, '/cobabuat/public') === 0) {
-    $path = substr($path, strlen('/cobabuat/public'));
-}
+
+
+
+// Choose public directory from  folder 'public' position in path
+$publicDir = getPublicDirectory($path);
+
+// Delete public directory from URI
+$path = removePublicDirectory($path, $publicDir);
+
+// Reset REQUEST_URI with choosen path
 $_SERVER['REQUEST_URI'] = '/' . trim($path, '/');
+
+// Function for get public directory berdasarkan position form folder 'public' in path
+function getPublicDirectory($path)
+{
+    $publicDir = '/public'; // default value public directory
+
+    // Find Position folder 'public' in path
+    $publicPos = strpos($path, '/public');
+
+    // If folder 'public' Found, set public directory from that position 
+    if ($publicPos !== false) {
+        $publicDir = substr($path, 0, $publicPos + strlen('/public'));
+    }
+
+    return $publicDir;
+}
+
+// Function for delete public directory from URI
+function removePublicDirectory($path, $publicDir)
+{
+    // Delete public directory from begining path if found
+    if (strpos($path, $publicDir) === 0) {
+        $path = substr($path, strlen($publicDir));
+    }
+
+    return $path;
+}
+
+$router = require_once APPPATH . '/Routes/routes.php';
 
 $router->run();
